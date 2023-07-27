d87460c3468b6dcb9ed4a50f9cf4606850068123
P Arvind <110770904+Arvind200323@users.noreply.github.com>
2023-07-27 18:22:29 +0530

    1431. Kids With the Greatest Number of Candies.cpp

---
+++ Leet Code/1431. Kids With the Greatest Number of Candies.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        int maxEle = candies[0];
+        int n = candies.size();
+        for(int i=0;i<n;i++){
+            if(candies[i] > maxEle){
+                maxEle = candies[i];
+            }
+        }
+        vector<bool> res;
+        for(int i=0;i<n;i++){
+            int x = candies[i];
+            x += extraCandies;
+            if(x >= maxEle){
+                res.push_back(true);
+            }else{
+                res.push_back(false);
+            }
+        }
+        return res;
+    }
+};
