fccbc3d3613445931338a8698eec97f0760217f7
P Arvind <110770904+Arvind200323@users.noreply.github.com>
2023-07-28 17:39:45 +0530

    Arrays

--- Leet Code/27. Remove Element.cpp
+++ Leet Code/27. Remove Element.cpp
@@ -5,7 +5,7 @@ public:
         int j = 0; // Initialize a pointer to keep track of the modified array
 
         for(int i = 0; i < n; i++) {
-            if(nums[i] == val) // If the current element is equal to the target value
+            if(nums[i] == val) // If the current element is equal to the target value 
                 continue; // Skip this element and move to the next iteration
 
             nums[j++] = nums[i]; // Copy the non-target element to the modified array and increment the pointer
