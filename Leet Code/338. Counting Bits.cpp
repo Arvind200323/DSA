046021bbcaf7fe1d646a043b63c4c0645f944448
P Arvind <110770904+Arvind200323@users.noreply.github.com>
2023-09-01 20:13:07 +0530

    Bit Manipulation | DP

---
+++ Leet Code/338. Counting Bits.cpp
@@ -0,0 +1,31 @@
+
+// o(nlogn) complexity solution
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> ans(n+1,0);
+        for(int i=0;i<=n;i++){
+            int num=i;
+            int count=0;
+            while(num){
+                count=count+num%2;
+                num=num/2;
+                ans[i]=count;
+            }
+        }
+        return ans;
+    }
+};
+
+// o(n) complexity solution
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> ans(n+1,0);
+        for(int i=0;i<=n;i++){
+            if(i%2==0) ans[i]=ans[i/2];
+            else ans[i]=ans[i/2]+1;    
+        }
+        return ans;
+    }
+};
