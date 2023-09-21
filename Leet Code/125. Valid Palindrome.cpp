869a60371935af875286ef646c32a2c6af5c75bb
P Arvind <110770904+Arvind200323@users.noreply.github.com>
2023-09-21 18:04:26 +0530

    String | Two Pointers

--- Leet Code/125. Valid Palindrome.cpp
+++ Leet Code/125. Valid Palindrome.cpp
@@ -4,7 +4,7 @@ public:
         int start = 0;
         int end = s.size()-1;
         while(start<=end){
-            if(!isalnum(s[start])){
+            if(!isalnum(s[start])){ 
                 start++;
                 continue;
             }if(!isalnum(s[end])){
