8753e3d91966b4f76e32428112ee409bb628c0f9
P Arvind <110770904+Arvind200323@users.noreply.github.com>
2024-01-01 21:46:06 +0530

    Hash Map |  STL

--- Leet Code/1436. Destination City.cpp
+++ Leet Code/1436. Destination City.cpp
@@ -4,7 +4,7 @@ public:
         unordered_map<string,string> mpp;
         for(int i=0;i<paths.size();i++){
             mpp[paths[i][0]] = paths[i][1];
-        } 
+        }  
         string dest = "$";
         string temp = paths[0][0];
         while(dest=="$"){
