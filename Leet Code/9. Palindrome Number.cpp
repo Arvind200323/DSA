2fca3c8579dd23807a91a594b784cef1ba35bef2
P Arvind <110770904+Arvind200323@users.noreply.github.com>
2024-01-10 10:54:47 +0530

    Math

--- Leet Code/9. Palindrome Number.cpp
+++ Leet Code/9. Palindrome Number.cpp
@@ -5,7 +5,7 @@ public:
          if(x<0) return false;
          long long int num=0;
          while(x){
-             num=num*10;
+             num=num*10; 
              num=num+x%10;
              x=x/10;
          }
