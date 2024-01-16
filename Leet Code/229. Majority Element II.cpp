d8e0e329827ec46a9043ed0e799c7f40aaa5d53a
P Arvind <110770904+Arvind200323@users.noreply.github.com>
2024-01-16 18:02:13 +0530

    Hash Map

--- Leet Code/229. Majority Element II.cpp
+++ Leet Code/229. Majority Element II.cpp
@@ -1,33 +1,14 @@
-// class Solution {
-// public:
-//     vector<int> majorityElement(vector<int>& nums) {
-//         vector<int> ans;
-//         unordered_map<int,int> mpp;
-//         for(int i=0;i<nums.size();i++){
-//             mpp[nums[i]]++;
-//         }
-//         for(int i=0;i<nums.size();i++){
-//             if(mpp[i]>=nums.size()/3){
-//                 int c=0;
-//                 if(c != nums[i]){
-//                     ans.push_back(nums[i]);  
-//                 }
-//                 c=nums[i];
-//             }
-//         }
-//         return ans;
-//     }
-// };
-
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        unordered_map<int, int> mp;
-        for(int num: nums) mp[num]++;
-        vector<int> result;
-        for(auto it = mp.begin(); it != mp.end(); it++){
-            if(it->second * 3 > nums.size()) result.push_back(it->first);
+        unordered_map<int,int> mpp;
+        vector<int> ans;
+        for(auto x:nums) mpp[x]++;
+        for(auto x:mpp){
+            if(x.second>nums.size()/3){
+                ans.push_back(x.first);
+            }
         }
-        return result;
+        return ans;
     }
 };
