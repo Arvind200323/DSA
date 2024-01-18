bf8e79a5f7567a0a7bfe91c9991fa4d75c989ac1
P Arvind <110770904+Arvind200323@users.noreply.github.com>
2024-01-18 16:45:32 +0530

    Arrays | Math

--- Leet Code/118. Pascal's Triangle.cpp
+++ Leet Code/118. Pascal's Triangle.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> result;
-        for (int i = 0; i < numRows; i++) {
-            vector<int> row(i + 1, 1);
-            for (int j = 1; j < i; j++) {
-                row[j] = result[i - 1][j - 1] + result[i - 1][j];
+        vector<vector<int>> ans;
+        vector<int> temp;
+        temp.push_back(1);
+        ans.push_back(temp);
+        for(int i=1;i<numRows;i++){
+            temp.push_back(0);
+            for(int j=i;j>0;j--){
+                temp[j]=temp[j]+temp[j-1];
             }
-            result.push_back(row);
+            ans.push_back(temp);
         }
-        return result;
+        return ans;
     }
 };
