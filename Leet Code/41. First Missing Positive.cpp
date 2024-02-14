5d7a9bf2c2218b215ffe76e5fe05efb8574d1e43
P Arvind <110770904+Arvind200323@users.noreply.github.com>
2024-02-14 19:34:39 +0530

    Hash Map

---
+++ Leet Code/41. First Missing Positive.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int ans=0, i=1;
+        unordered_map<int,int> mpp;
+        for(auto x:nums) mpp[x]++;
+        while(ans==0){
+            if(mpp[i]==0) ans=i;
+            else i++;
+        }
+        return ans;
+    }
+};
