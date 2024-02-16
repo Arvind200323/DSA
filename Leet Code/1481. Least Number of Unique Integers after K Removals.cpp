6f6c768c5c78639c5f07d2c47c90abc4e71b4e86
P Arvind <110770904+Arvind200323@users.noreply.github.com>
2024-02-16 18:14:47 +0530

    Hash Map | Priority Queue

---
+++ Leet Code/1481. Least Number of Unique Integers after K Removals.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findLeastNumOfUniqueInts(vector<int>& arr, int k) {
+        unordered_map<int,int> mpp;
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+        for(auto x:arr) mpp[x]++;
+        for(auto x:mpp) pq.push({x.second,x.first});
+        while(true){
+            int a = pq.top().first;
+            if(a<=k) {
+                k -= a;
+                pq.pop();
+            }
+            else break;
+        }
+        return pq.size();
+    }
+};
