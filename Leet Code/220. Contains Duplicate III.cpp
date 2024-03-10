b895226e31e009a23f65f3ddc12e2b3238607adf
P Arvind <110770904+Arvind200323@users.noreply.github.com>
2024-03-10 12:00:24 +0530

    Sets | Lower Bound | STL

---
+++ Leet Code/220. Contains Duplicate III.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool containsNearbyAlmostDuplicate(vector<int>& nums, int indexDiff, int valueDiff) {
+        // for(int i=0;i<nums.size();i++){
+        //     for(int j=i+1;j<nums.size();j++){
+        //         if(abs(i-j) <= indexDiff && abs(nums[i]-nums[j]) <= valueDiff){
+        //             return true;
+        //         }
+        //     }
+        // }
+        // return false;
+
+        // int left=0, right=1;
+        // while(left<right && right<nums.size()){
+        //     if(abs(left-right) <= indexDiff){
+        //         if(abs(nums[left]-nums[right]) <= valueDiff) return true;
+        //         right++;
+        //     }
+        //     else left++;
+        // }
+        // return false;
+
+        set<int> st;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(i>indexDiff) st.erase(nums[i-indexDiff-1]);
+            auto lb=st.lower_bound(nums[i]-valueDiff);
+            if(lb!=st.end() && *lb-nums[i]<=valueDiff) return true;
+            st.insert(nums[i]);
+        }
+        return false;
+    }
+};
