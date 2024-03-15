6526f83f5452bb1e8b66d548035c285c95b74e07
P Arvind <110770904+Arvind200323@users.noreply.github.com>
2024-03-15 20:18:35 +0530

    Prefix Sum

--- Leet Code/238. Product of Array Except Self.cpp
+++ Leet Code/238. Product of Array Except Self.cpp
@@ -17,27 +17,24 @@ public:
         // return ans;
 
         int n = nums.size();
-
-        vector<int> left(n);
-        vector<int> right(n);
         vector<int> ans(n);
 
-        left[0] = nums[0];
-        for(int i=1;i<n;i++){
-            left[i] = nums[i]*left[i-1];
-        }
-
-        right[n-1] = nums[n-1];
-        for(int i=n-2;i >0;i--){
-            right[i] = nums[i]*right[i+1];
-        }
-
-        ans[0] = right[1];
-        ans[n-1] = left[n-2];
-        for(int i=1;i<n-1;i++){
-            ans[i] = left[i-1]*right[i+1];
+        int zero = 0;
+        int prod = 1;
+        for(int i=0;i<n;i++){
+            if(nums[i]==0) zero++;
+            else prod *= nums[i];
         }
         
+        for(int i=0;i<n;i++){
+            if(zero==0) ans[i] = prod/nums[i];
+            else{
+                if(zero==1){
+                    if(nums[i]==0) ans[i] = prod;
+                    else ans[i]=0;
+                }
+            }
+        }
         return ans;
     }
 };
