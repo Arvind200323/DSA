10379c4db189fc0aedd00c23441ecc2d828280f9
P Arvind <110770904+Arvind200323@users.noreply.github.com>
2024-03-24 10:08:31 +0530

    Floyd's Cycle

---
+++ Leet Code/287. Find the Duplicate Number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+        while(nums[0] != nums[nums[0]]){
+            swap(nums[0],nums[nums[0]]);
+        }
+        return nums[0];
+    }
+};
