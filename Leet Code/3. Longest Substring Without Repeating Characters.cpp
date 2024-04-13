4e30bf58cad3d7eb2df183f805e5d1308d8eb32f
P Arvind <110770904+Arvind200323@users.noreply.github.com>
2024-04-13 14:18:22 +0530

    Sliding Window | Hash Map

---
+++ Leet Code/3. Longest Substring Without Repeating Characters.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_map<char,int> mpp;
+        int i=0;
+        int ans=0;
+        for(int j=0;j<s.length();j++){
+            mpp[s[j]]++;
+            while(mpp[s[j]]>1){
+                mpp[s[i]]--;
+                i++;
+            }
+            ans=max(ans,j-i+1);
+        }
+        return ans;
+    }
+};
