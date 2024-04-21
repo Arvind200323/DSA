ead5ea635794d81f98c2489e1c61cc574cc96a34
P Arvind <110770904+Arvind200323@users.noreply.github.com>
2024-04-21 09:12:59 +0530

    Linked List

---
+++ Leet Code/148. Sort List.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) {
+        vector<int> arr;
+        ListNode* temp=head;
+        while(temp!=NULL){
+            arr.push_back(temp->val);
+            temp=temp->next;
+        }
+        sort(arr.rbegin(),arr.rend());
+        temp=head;
+        while(!arr.empty()){
+            temp->val = arr.back();
+            temp=temp->next;
+            arr.pop_back();
+        }
+        return head;
+    }
+};
